86072225fe655dab0d3478b64ae8e1701ae9627a
CaffeinatedGeek <wxwsk8er@gmail.com>
2016-03-07 16:05:03 -0500

    Major update for Beta release. Nearly ready for beta! Added: Player inventory, Player lives, Player character, Improved player movement, New texture pack (now 32x32 px), added new player sprite rendering, added entity inventories, Added Items,  New tiles: Sand, Sandstone, Present, Steel, Stars, Player Left, Player Right

--- Upload.sh
+++ Upload.sh
@@ -4,4 +4,4 @@ git add .
 git commit -m "$Message"
 git remote remove origin
 git remote add origin https://gitlab.com/Plexian/Pendulum.git
-git push -u origin master
+git push -u origin master --force
