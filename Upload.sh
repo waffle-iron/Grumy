6329d8d089bbfab7b5a4cf2d3297a46f1255e7dc
CaffeinatedGeek <wxwsk8er@gmail.com>
2016-04-13 13:19:35 -0400

    Upload

--- Upload.sh
+++ Upload.sh
@@ -2,4 +2,4 @@ git add .
 git commit -m "Upload"
 git remote remove origin
 git remote add origin https://gitlab.com/Plexian/Pendulum.git
-git push -u origin master
+git push -u origin master --force
